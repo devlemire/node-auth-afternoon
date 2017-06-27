5424597f2b98055c3ec435419c78fb5aa40fd783
James Lemire <webdev.jameslemire@gmail.com>
2017-06-27 15:48:57 -0600

    JL

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const request = require('request');
 
 const app = express();
 app.use( session({
@@ -7,6 +8,21 @@ app.use( session({
   resave: false,
   saveUninitialized: false
 }));
+
+const GitHubRequest = {
+  url: 'https://api.github.com/user/followers',
+  headers: {
+    'User-Agent': 'request'
+  }
+}
+
+app.get('/followers', ( req, res, next ) => {
+  request(GitHubRequest, ( error, response, body ) => {
+    console.log('Error', error);
+    // console.log('Response,', response);
+    console.log('Body', body);
+  });
+});
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
---
+++ strategy.js
@@ -0,0 +1,16 @@
+
+const Auth0Strategy = require('passport-auth0');
+
+module.exports = new Auth0Strategy({
+   domain:       'jameslemire.auth0.com',
+   clientID:     '4_8ZQzEOP6mYeoQbeAmscWFmjl-SjIVt',
+   clientSecret: '0jst7L31Ygbol37KTMTV504h-OO73anLV_wAoRIsueBgCVR83gtV4vE6FpZJaUhW',
+   callbackURL:  '/login'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
